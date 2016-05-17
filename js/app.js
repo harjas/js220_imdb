5035d53feb3d3e12bad8da8bfd7048136dabea92
harjas <harjassinghglobal@gmail.com>
2016-05-16 19:10:09 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//This is where application config goes
+angular.module('<app name goes here>', ['<add angular router here']);
+
+angular.module('<app name goes here>')
+  .config(['$routeProvider', function(<add missing parameter>) {
+    //add routes named list, gallery and details
+    //application should route back to list if anything else is entered 
+  }])
