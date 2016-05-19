36368e174ce3e354bce774fecb4137b52d273612
Ronnie Ho <ronho@expedia.com>
2016-05-18 19:34:13 -0700

    Added routing inside app.js and updated index.html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 //This is where application config goes
-angular.module('<app name goes here>', ['<add angular router here']);
+angular.module('imdbApp', ['ngRoute']);
 
-angular.module('<app name goes here>')
-  .config(['$routeProvider', function(<add missing parameter>) {
+angular.module('imdbApp')
+  .config(['$routeProvider', function($routeProvider) {
     //add routes named list, gallery and details
+    $routeProvider.when('/list', {
+    	templateUrl: 'templates/list.html'
+    })
+    .when('/gallery', {
+    	templateUrl: 'templates/gallery.html'
+    })
+    .when('/details', {
+    	templateUrl: 'templates/details.html'
+    })
+
     //application should route back to list if anything else is entered 
-  }])
+    .otherwise({
+    	redirectTo: '/'
+    });
+  }]);
