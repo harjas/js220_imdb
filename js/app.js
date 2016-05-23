48deb5e5e1b382aa6f8fbc1e03e0a03209f1ee29
David Kovit <dkovit@expedia.com>
2016-05-23 11:33:58 -0700

    Homework week 4
    Setting up routing for imdb movie app

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 //This is where application config goes
-angular.module('<app name goes here>', ['<add angular router here']);
+angular.module('movie', ['ngRoute']);
 
-angular.module('<app name goes here>')
-  .config(['$routeProvider', function(<add missing parameter>) {
+angular.module('movie')
+  .config(['$routeProvider',function($routeProvider) {
     //add routes named list, gallery and details
-    //application should route back to list if anything else is entered 
+    //application should route back to list if anything else is entered
+    $routeProvider.when('/',{
+      templateUrl: '/templates/list.html'
+    })
+    .when('/list',{
+      templateUrl: '/templates/list.html'
+    })
+    .when('/gallery',{
+      templateUrl: '/templates/gallery.html'
+    })
+    .when('/details',{
+      templateUrl: '/templates/details.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
   }])
