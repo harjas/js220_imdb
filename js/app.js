9f37e9851d829eaa71354926adb3c45ee497f74c
harjas <harjassinghglobal@gmail.com>
2016-05-25 11:27:27 -0400

    added imdb json and images

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 //This is where application config goes
-angular.module('<app name goes here>', ['<add angular router here']);
+angular.module('imdbApp', ['ngRoute', 'services']);
 
-angular.module('<app name goes here>')
-  .config(['$routeProvider', function(<add missing parameter>) {
+angular.module('imdbApp')
+  .config(['$routeProvider', function($routeProvider) {
     //add routes named list, gallery and details
-    //application should route back to list if anything else is entered 
+    //application should route back to list if anything else is entered
+    $routeProvider.when('/gallery', {
+      templateUrl: 'templates/gallery.html'
+    })
+    .when('/list', {
+      templateUrl: 'templates/list.html',
+      controller: 'listController'
+    })
+    .when('/details', {
+      templateUrl:'templates/details.html'
+    })
+    .when('/', {
+      templateUrl:'templates/list.html'
+    })
+    .otherwise({
+      redirectTo: '/list'
+    })
   }])
