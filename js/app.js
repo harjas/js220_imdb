dcb37ee88d596de4d50cb5a8a2036ab368c9fdae
harjas <harjassinghglobal@gmail.com>
2016-05-25 11:31:31 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -5,20 +5,5 @@ angular.module('imdbApp')
   .config(['$routeProvider', function($routeProvider) {
     //add routes named list, gallery and details
     //application should route back to list if anything else is entered
-    $routeProvider.when('/gallery', {
-      templateUrl: 'templates/gallery.html'
-    })
-    .when('/list', {
-      templateUrl: 'templates/list.html',
-      controller: 'listController'
-    })
-    .when('/details', {
-      templateUrl:'templates/details.html'
-    })
-    .when('/', {
-      templateUrl:'templates/list.html'
-    })
-    .otherwise({
-      redirectTo: '/list'
-    })
+  
   }])
